2f640f7bd6ec6151aaa57d96e7405e2f2fa43160
aswini4006 <aswiniayya1999@gmail.com>
2025-05-25 16:02:02 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,31 @@
+//addition
+let a=100;
+let b=20;
+let c;
+c=a+b;
+console.log(c);
+//subtraction
+let D=100;
+let E=50;
+let F;
+F=D-E;
+console.log(F);
+//multiplication
+let G=54;
+let H=2;
+let I;
+I=G*H;
+console.log(I);
+//division
+let J=100;
+let K=5;
+let L;
+L=J/K;
+console.log(L);
+//square
+let number = 5;
+let square = number ** 2;
+console.log(square);
+//cube
+const cube = (num) => num * num * num;
+console.log(cube(3));
